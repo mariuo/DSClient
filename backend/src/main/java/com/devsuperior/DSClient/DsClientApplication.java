3230d37f04d231e90aea5fc7d352ce27fd13f905
Mario Camelo <mariuo@gmail.com>
2021-01-21 10:19:14 -0300

    Projet Created

---
+++ backend/src/main/java/com/devsuperior/DSClient/DsClientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsClientApplication.class, args);
+	}
+
+}
